da0f814a19eeddbf354d3e8124f196ee8ac41725
einekratzekatze <einekratzekatze@posteo.eu>
2023-10-11 12:24:30 +0200

    added method to obtain IDs from the switchbrew wiki and ndeadly's GitHub

--- main.py
+++ main.py
@@ -1,146 +1,51 @@
+import json
 import os
 from configparser import ConfigParser
+import pandas as pd
+from github import Github
+from dataclasses import dataclass
 
 
-def main():
-    title_id_dict = {
-        "0100000000010000": "your theme",
-        "0100000000000000": "fs",
-        "0100000000000001": "ldr",
-        "0100000000000002": "ncm",
-        "0100000000000003": "pm",
-        "0100000000000004": "sm",
-        "0100000000000005": "boot",
-        "0100000000000006": "usb",
-        "0100000000000007": "tma.stub/htc.stub",
-        "0100000000000008": "boot2",
-        "0100000000000009": "settings",
-        "010000000000000A": "Bus",
-        "010000000000000B": "bluetooth",
-        "010000000000000C": "bcat",
-        "010000000000000D": "dmnt",
-        "010000000000000E": "friends",
-        "010000000000000F": "nifm",
-        "0100000000000010": "ptm",
-        "0100000000000011": "shell",
-        "0100000000000012": "bsdsockets",
-        "0100000000000013": "hid",
-        "0100000000000014": "audio",
-        "0100000000000015": "LogManager",
-        "0100000000000016": "wlan",
-        "0100000000000017": "cs",
-        "0100000000000018": "ldn",
-        "0100000000000019": "nvservices",
-        "010000000000001A": "pcv",
-        "010000000000001B": "ppc/capmtp",
-        "010000000000001C": "nvnflinger",
-        "010000000000001D": "pcie/pcie.withoutHb",
-        "010000000000001E": "account",
-        "010000000000001F": "ns",
-        "0100000000000020": "nfc",
-        "0100000000000021": "psc",
-        "0100000000000022": "capsrv",
-        "0100000000000023": "am",
-        "0100000000000024": "ssl",
-        "0100000000000025": "nim",
-        "0100000000000026": "cec",
-        "0100000000000027": "tspm",
-        "0100000000000028": "spl",
-        "0100000000000029": "lbl",
-        "010000000000002A": "btm",
-        "010000000000002B": "erpt",
-        "010000000000002C": "time",
-        "010000000000002D": "vi",
-        "010000000000002E": "pctl",
-        "010000000000002F": "npns",
-        "0100000000000030": "eupld",
-        "0100000000000031": "arp/glue",
-        "0100000000000032": "eclct",
-        "0100000000000033": "es",
-        "0100000000000034": "fatal",
-        "0100000000000035": "grc",
-        "0100000000000036": "creport",
-        "0100000000000037": "ro",
-        "0100000000000038": "profiler",
-        "0100000000000039": "sdb",
-        "010000000000003A": "migration",
-        "010000000000003B": "jit",
-        "010000000000003C": "jpegdec",
-        "010000000000003D": "safemode",
-        "010000000000003E": "olsc",
-        "010000000000003F": "dt",
-        "0100000000000040": "nd",
-        "0100000000000041": "ngct",
-        "0100000000000042": "pgl",
-        "0100000000000045": "omm",
-        "0100000000000046": "eth",
-        # homebrew
-        "0000000000534C56": "SaltyNX",
-        "00FF0000000002AA": "BootSoundNX",
-        "00FF0000636C6BF2": "nx-reader",
-        "00FF0000636C6BFF": "sys-clk",
-        "00FF00006D7470FF": "mtp-server-nx",
-        "00FF0000A53BB665": "SysDVR",
-        "00FF747765616BFF": "switch-sys-tweak",
-        "0100000000000052": "switch-nfp-mitm",
-        "0100000000000081": "nx-btred",
-        "0100000000000352": "emuiibo",
-        "0100000000000464": "SwitchPresence",
-        "0100000000000523": "aoc-mitm",
-        "0100000000000901": "OJDS-NX",
-        "0100000000000BED": "vax",
-        "0100000000000BEF": "fsp-usb",
-        "0100000000000dad": "nx_overlay",
-        "0100000000000f12": "Fizeau",
-        "0100000000000faf": "hid-mitm/hid-mitm-plus",
-        "0100000000006480": "twili",
-        "0100000000007200": "ilia",
-        "010000000000bd00": "MissionControl",
-        "010000000000C235": "Freebird",
-        "010000000000f00f": "dvdnx",
-        "010000000000FFAB": "usb-mitm",
-        "01000000001ED1ED": "maydel",
-        "0532232232232000": "NX-input-recorder",
-        "054e4f4558454000": "noexs",
-        "2200000000000100": "SplitNX",
-        "4100000000000324": "sys-http",
-        "4200000000000000": "sys-tune/sys-audioplayer",
-        "420000000000000E": "sys-ftpd/sys-ftpd-light",
-        "420000000000000F": "SlideNX",
-        "4200000000000010": "ldn_mitm",
-        "4200000000000811": "bitmap-printer",
-        "4200000000000BA6": "BTSounds",
-        "4200000000000BAC": "SwiTAS",
-        "4200000000000FFF": "sys-triplayer",
-        "420000000007E51A": "nx-ovlloader",
-        "4200000000474442": "sys-gdbstub",
-        "4200000000696969": "sys-logger",
-        "4200000AF1E8DA89": "ControllerSaver",
-        "42000062616B6101": "sys-screenuploader",
-        "4200736372697074": "sys-script",
-        "4206900000000012": "sfdnsres_mitm",
-        "430000000000000A": "sys-netcheat",
-        "430000000000000B": "sys-botbase",
-        "430000000000000C": "sys-botbaseplus",
-        "43000000000000FF": "nxsh",
-        "4300000000000909": "NXGallery",
-        "5600000000000000": "NXCord",
-        "690000000000000D": "sys-con"
-    }
+def print_dict_content(dict_to_print):
+    """for debugging"""
+    for values in dict_to_print.values():
+        print(values)
 
-    config = ConfigParser()
-    config.read('config.ini')
-    path = config.get('config', 'path')
-    allow_user_input = config.getboolean('config', 'allow_user_input')
-    allow_path_input = config.getboolean('config', 'allow_path_input')
-    if not allow_path_input and not allow_path_input:
-        print("Allow at least one input")
-        return
 
+@dataclass
+class ImportWebsites:
+    url: str
+    end: str  # placed at the end of the printed message
+    table_names: tuple
+    id_dict: dict
+    debug_mode: bool
+
+    def request_tables(self):
+        tables = pd.read_html(self.url)  # Returns list of all tables on page.
+
+        for counter, table in enumerate(tables):
+            dict_table = table.to_dict('records')
+            for entry in dict_table:
+                has_name = 'Name' in entry and entry['Name'] == entry['Name']
+                has_desc = 'Description' in entry and entry['Description'] == entry['Description']
+                has_notes = 'Notes' in entry and entry['Notes'] == entry['Notes']
+
+                self.id_dict[entry['ProgramId']] = \
+                    (f"The ID {entry['ProgramId']} is part of the category \"{self.table_names[counter]}\". "
+                     f"{f"It is described as \"{entry['Name']}\". " if has_name else ""}"
+                     f"{f"It is described as \"{entry['Description']}\". " if has_desc else ""}"
+                     f"{f"Note: {entry['Notes']}" if has_notes else ""}"
+                     f"{"But there is no information about this ID." if not (has_name or has_desc or has_notes) else ""}"
+                     f"{self.end if self.end else ""}")
+        return self.id_dict
+
+
+def print_user_list(title_id_dict, path, allow_path_input, allow_user_input):
     ids = []
     if allow_path_input:
         try:
-            ids.extend([name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))])
+            ids.extend(
+                [folder_name for folder_name in os.listdir(path) if os.path.isdir(os.path.join(path, folder_name))])
         except FileNotFoundError:
             print(f"No such file or directory: {path}")
     if allow_user_input:
@@ -155,7 +60,7 @@ def main():
 
 def get_user_input():
     input_list = []
-    print("Enter file names one per line, leave empty to proceed")
+    print("Enter folder names/title ids one per line, leave empty to proceed")
     while True:
         user_input = input()
         if user_input:
@@ -163,6 +68,61 @@ def get_user_input():
         else:
             break
     return input_list
+
+
+def main():
+    # loading config
+    config = ConfigParser()
+    config.read('config.ini')
+    path = config.get('config', 'path')
+    allow_path_input = config.getboolean('config', 'allow_path_input')
+    allow_user_input = config.getboolean('config', 'allow_user_input')
+    obtain_data = config.getboolean('config', 'obtain_data')
+    debug_mode = config.getboolean('config', 'debug_mode')
+
+    if obtain_data:
+        # obtain Homebrew sysmodules from ndeadly
+        id_dict = {}
+        raw_gist = Github().get_gist("a4b8c01bb453028cd0008f282098f696").raw_data['files']['sysmodules.txt']['content']
+        for sysmodule in raw_gist.split("/* Homebrew sysmodules */\n")[-1].splitlines():
+            l = sysmodule.split("	")
+            id_dict[l[
+                0]] = f"The ID {l[0]} is the ID for the Homebrew sysmodule {l[1]}. Check it out on GitHub for more information."
+        # obtains Nintendo sysmodules from the switchbrew wiki
+        id_dict = id_dict = ImportWebsites(
+            url=r'https://switchbrew.org/wiki/Title_list/Games',
+            end=f"Note: if this ID is in your /atmosphere/contents folder, the content is likely a mod.",
+            table_names=(
+                "Applications / Games", "Demos"),
+            debug_mode=debug_mode,
+            id_dict=id_dict).request_tables()
+        id_dict = ImportWebsites(
+            url=r'https://switchbrew.org/wiki/Title_list',
+            end="",
+            table_names=(
+                "System Modules", "System Data Archives", "System Applets",
+                "Development System Applets",
+                "Debug System Modules", "Development System Modules", "Bdk System Modules",
+                "New Development System Modules", "System Applications",
+                "Pre-release System Applets",
+                "Pre-release System Modules", "User Applications"),
+            debug_mode=debug_mode,
+            id_dict=id_dict).request_tables()
+        if debug_mode:
+            print_dict_content(id_dict)
+        with open('title-ids.json', 'w') as fp:
+            json.dump(id_dict, fp, indent=2)
+    else:
+        try:
+            id_dict = json.load(open('title-ids.json'))
+        except FileNotFoundError:
+            print("The file title-ids.json could not be found. Please change \"obtain_data\" in \"config.ini\" to True.")
+            return
+
+    if allow_path_input or allow_user_input:
+        print_user_list(id_dict, path, allow_path_input, allow_user_input)
+    else:
+        print("Allow at least one input type in the config")
 
 
 if __name__ == '__main__':
